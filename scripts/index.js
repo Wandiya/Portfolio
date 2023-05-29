09051f6cbb2c3a09107b9b82748aadcf23f7fd45
Wandiya James <jameswandiya@gmail.com>
2023-05-29 14:33:54 +0100

    add scroll reveal and animation

--- scripts/index.js
+++ scripts/index.js
@@ -35,3 +35,13 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 // scroll reveal section
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portforlio-box, contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content, h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content, p, .about-content', { origin: 'right' });
