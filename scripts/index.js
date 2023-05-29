afe00a19027f6829263744b4e58396828bb6c15b
Wandiya James <jameswandiya@gmail.com>
2023-05-29 21:33:07 +0100

    add typed.js styles

--- scripts/index.js
+++ scripts/index.js
@@ -45,3 +45,13 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portforlio-box, contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content, h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content, p, .about-content', { origin: 'right' });
+
+// typed.js
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Youtuber', 'Blogger', 'Problem Solver'],
+    backspeed: 0,
+    typespeed: 55,
+    backDelay: 1000,
+    loop: true,
+    showCursor: false,
+});
